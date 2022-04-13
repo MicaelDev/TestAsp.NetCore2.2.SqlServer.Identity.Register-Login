f177731430679f9faaf5b404c52615bb5e67c776
Micael20 <74980510+MicaelDev@users.noreply.github.com>
2022-04-12 22:51:41 -0300

    Adicionar arquivos de projeto.

---
+++ HospitalManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HospitalManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
